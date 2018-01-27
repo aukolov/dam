b7c28bcad5925621a81de08fc8f33f5c803ee497
aukolov <aukolov@quotix.com>
2018-01-27 14:38:24 +0200

    Save and show data from last year #4 - displaying data

---
+++ Dam.Web/Models/DamRowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Dam.Domain;
+
+namespace Dam.Web.Models
+{
+    public class DamRowViewModel
+    {
+        public DamRowViewModel(DamSnapshot currentSnapshot, DamSnapshot lastYearsSnapshot)
+        {
+            CurrentSnapshot = currentSnapshot;
+            LastYearsSnapshot = lastYearsSnapshot;
+        }
+
+        public DamSnapshot CurrentSnapshot { get; }
+        public DamSnapshot LastYearsSnapshot { get; }
+    }
+}
--- Dam.Web/Models/HomeViewModel.cs
+++ Dam.Web/Models/HomeViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Dam.Domain;
 using Dam.Infrastructure.DataAccess;
 using Dam.Web.Extensions;
 
@@ -12,14 +11,18 @@ namespace Dam.Web.Models
         public HomeViewModel()
         {
             var damSnapshotRepository = new DamSnapshotRepository(() => new DataContext());
-            LatestSnapshots = damSnapshotRepository.Items.GroupBy(x => x.Dam)
+            var latestSnapshots = damSnapshotRepository.Items.GroupBy(x => x.Dam)
                 .Select(x => x.MaxBy(snapshot => snapshot.Date))
                 .OrderByDescending(snapshot => snapshot.Dam.Capacity)
                 .ToList();
-            LastUpdate = LatestSnapshots.Max(snapshot => snapshot.Date);
+            DamRows = latestSnapshots.Select(x => new DamRowViewModel(x,
+                  damSnapshotRepository.Items
+                  .SingleOrDefault(y => x.Dam == y.Dam && x.Date.AddYears(-1) == y.Date)))
+                .ToList();
+            LastUpdate = latestSnapshots.Max(snapshot => snapshot.Date);
         }
 
-        public List<DamSnapshot> LatestSnapshots { get; }
+        public List<DamRowViewModel> DamRows { get; }
         public DateTime LastUpdate { get; }
     }
 }
